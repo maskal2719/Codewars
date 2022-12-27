d4af6a52a6a3d329a597c7ccd4bcef9c974ad85f
Stanislav Moskalenko <maskal2719@gmail.com>
2022-12-27 11:11:36 +0300

    feat: add task  Vowel One

---
+++ 7 Kyu/vowel_one.js
@@ -0,0 +1,5 @@
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+function vowelOne(s){
+    return s.replace(/[^aeiou]/gi, '0').replace(/[^\d]/g,'1');
+  }
