56f685133475bf1aa44b721f1c2ee0a613160c09
Stanislav Moskalenko <maskal2719@gmail.com>
2023-01-19 03:21:09 +0300

    fix: delete

--- 8 Kyu/maximum_and_minimum.js
+++ 8 Kyu/maximum_and_minimum.js
@@ -1,4 +1,4 @@
-// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+// Your task is to make tw functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
 
 var min = function(list){
     return Math.min.apply(null,list)
